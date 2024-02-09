5646446c23840a62bfea4cfbdbe3aeeede09118f
Siyaka Fatimat <siyakafatimat@gmail.com>
2024-02-09 14:00:01 +0100

    UI

---
+++ components/BusinessDetails.tsx
@@ -0,0 +1,17 @@
+const BusinessDetails = () => {
+  return (
+    <>
+      <div className="container mt-10">
+        <h1 className="text-center font-bold text-[25px] py-20">
+          Your Business Details
+        </h1>
+
+        <div className="ParentContainer bg-[#222] w-full h-[500px] m-auto flex items-center justify-center">
+          <div className="parent bg-[#333] w-[96%] h-[460px] rounded-2xl m-auto"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BusinessDetails;
